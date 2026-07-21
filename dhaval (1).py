893981caeb780791d5d95229408781bb726899ef
dhaval5192 <dhav2393@gmail.com>
2026-07-21 19:34:03 +0530

    Add files via upload

---
+++ dhaval (1).py
@@ -0,0 +1,70 @@
+#  print("dhaval")
+#  print("adani foundation")
+#  print("course data analytics")
+
+# name =input("your name")
+# age =input("your age")
+# print("my name is", name)
+# print("my age is",age)
+
+# name =input("your name")
+# course = input("your course")
+# fees = input("your fees")
+# print (name,course,fees)
+
+# student_name = "dhaval"
+# marks=70
+# passed=True
+# print(student_name)
+# print(marks)
+# print(passed)
+
+# a=10
+# b=20
+# c=30
+# a,b=b,a
+# print("a=",a)
+# print("b=",b)
+
+# a=10
+# b=20
+# temp=a
+# a=b
+# b=temp
+# print("a=",a)
+# print("b=",b)
+
+# num = 10 
+# decimal = 5.5 
+# text = "data analytics"
+# status = True
+# print(int(num))
+# print(float(decimal))
+# print(str(text))
+# print(bool(status)) 
+
+# age = int(input("enter age:"))
+# salary = float(input("enter salary:"))
+# print("age:", type(age))
+# print("sellery:", type(salary))
+
+# x = 10
+# y = 5.5
+# result = x+ y
+# print(result)
+# print (float(result))
+
+# x = 10
+# print(type(x))
+
+# x = 10 + 5.5
+
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+print("addition=",num1 + num2)
+print("substraction=",num1 - num2)
+print("multiplication=",num1 * num2)
+print("addition=",num1 + num2)
+print("addition=",num1 + num2)
+
+
