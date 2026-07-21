1afe32a8ee6e9c964bb67a74ab2b264ff618330b
dhaval5192 <dhav2393@gmail.com>
2026-07-21 19:36:01 +0530

    Add files via upload

---
+++ lacture 3.py
@@ -0,0 +1,121 @@
+# print("Vansh")
+# print("Ahmedabad")
+# print("Welcome to Data Analytics")
+
+# name=input("Your name")
+# age=input("Your age")
+# print("My name is",name)
+# print("My age is",age)
+
+# name=input("Your name")
+# course=input("Your course")
+# fees=input("Your fees")
+# print(name,course,fees)\
+
+# student_name="vansh"
+# marks=85
+# passed=True
+# print(student_name)
+# print(marks)
+# print(passed)
+
+# a=10
+# b=20
+# temp=a
+# a=b
+# b=temp
+# print("a=",a)
+# print("b=",b)
+
+# num = 10
+# decimal = 5.5
+# text = "Data Analytics"
+# status = True
+# print(int(num))
+# print(float(decimal))
+# print(str(text))
+# print(bool(status))
+
+# age = int(input("enter age:"))
+# salary = float(input("enter salary:"))
+# print("age:", type(age))
+# print("salary:", type(salary))
+
+# import calendar 
+
+# year=int(input("enter year: "))
+# month=int(input("enter month: "))
+
+# cal=calendar . month (year,month)
+# print(cal)
+
+# x = 10
+# y = 5.5
+# result = x + y
+# print(result)
+# print(float(result))
+
+# num1=float(input("enter first number:"))
+# num2=float(input("enter second number:"))
+# print("addition =",num1 + num2)
+# print("substraction =",num1 - num2)
+# print("multiplication =",num1 * num2)
+# print("division =",num1 / num2)
+# print("modulas =", num1 % num2)
+
+# num1=float(input("enter first number:"))
+# num2=float(input("enter second number:"))
+
+# if num1 > num2:
+#     print("greater number :", num1)
+#     print("smaller number :", num2)
+# elif num1 < num2: 
+#     print("greater number :", num1)
+#     print("smaller number :", num2)
+# else :
+#     print("both numbers are equal :", num1)
+
+# a = input("Enter True/False: ") == "True"
+# b = input("Enter True/False: ") == "True"
+# print("AND =", a and b)
+# print("OR =", a or b)
+# print("NOT A =", not a)
+
+# num = int(input("Enter Number: "))
+# num += 5
+# print("After +=5:", num)
+# num -= 2
+# print("After -=2:", num)
+# num *= 3
+# print("After *=3:", num)
+
+
+# text = input("Enter String: ")
+# char = input("Enter Character: ")
+# print(char in text)
+
+
+# for i in range(101):
+#     print(i)
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+# num = 5
+# for i in range(1, 11):
+#     print(num * i)
+
+# num = 456
+# rev = 0
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num = num // 10
+# print(rev)
+
+
+for i in range(5):
+    if i == 4:
+        break
+    print(i)
---
+++ t1.py
@@ -0,0 +1,79 @@
+# print("My name is Anurag ")
+# print("I am from Ahmedabad")
+# print("Welcome to Data Analytics")
+
+# name=input("Your Name: ")
+# age=input("Your Age: ")
+# print("My name is ",name)
+# print("My age is ",age)
+
+# name = input("Enter Name: ")
+# course = input("Enter Course: ")
+# fees = input("Enter Fees: ")
+# print(name, course, fees)
+
+# student_name = "Anurag"
+# marks = 85
+# passed = True
+# print(student_name)
+# print(marks)
+# print(passed)
+
+# a, b, c = 10, 20, 30
+# print(a, b, c)
+
+# a = 10
+# b = 20
+# a, b = b, a
+# print("a =", a)
+# print("b =", b)
+
+# num = 10
+# decimal = 5.5
+# text = "Data Analytics"
+# status = True
+# print(int(num))
+# print(float(decimal))
+# print(str(text))
+# print(bool(status))
+
+
+
+# x = 10
+# y = 5.5
+# result = x + y
+# print(result)
+# print(float(result))
+
+# num1 = float(input("Enter First Number: "))
+# num2 = float(input("Enter Second Number: "))
+# print("Addition =", num1 + num2)
+# print("Subtraction =", num1 - num2)
+# print("Multiplication =", num1 * num2)
+# print("Division =", num1 / num2)
+# print("Modulus =", num1 % num2)
+
+
+# a = input("Enter True/False: ") == "True"
+# b = input("Enter True/False: ") == "True"
+# print("AND =", a and b)
+# print("OR =", a or b)
+# print("NOT A =", not a)
+
+# num = int(input("Enter Number: "))
+# num += 5
+# print("After +=5:", num)
+# num -= 2
+# print("After -=2:", num)
+# num *= 3
+# print("After *=3:", num)
+
+# text = input("Enter String: ")
+# char = input("Enter Character: ")
+# print(char in text)
+
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+print(a is b)
+print(a is not c)
